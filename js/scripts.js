2cea77c5880d11c3534d7ee22dfec0b9007be003
“Paul <prowori2@gmail.com>
2019-03-26 15:00:25 +0300

    Add comments to the code files

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,12 @@
 -Make a form that gathers the users name and has multiple choice questions.
 -Write an algorithm that uses the input of the multiple choice questions to
  narrow down to one of several destinations
--Give output in the form of an answer suggesting a vacation destination.
--Once that is working fine, add animations and effects and CSS.
+-Give output in the form of an answer suggesting several vacation destinations.
+-
 
 */
 
 $(document).ready(function(){
-  var userInput = []
-  var textInput = $("#name").val();
-
 
   $('#young').click(function(){
     var textInput = $("#name").val();
@@ -18,6 +15,7 @@ $(document).ready(function(){
     $('#added1').remove();
     $('#added2').remove();
     $('<i id = "added1"> Maybe you should fill this form with an adult, ' + textInput + '</i>').insertAfter("#yet");
+    //Teases the user with comments in the age question
 
   });
 
@@ -27,6 +25,7 @@ $(document).ready(function(){
     $('#added1').remove();
     $('#added2').remove();
     $('<i id = "added"> That\'s a bit shady, but we won\'t judge you, ' + textInput + '</i>').insertAfter("#judge");
+    //Teases the user with comments in the age question
 
   });
 
@@ -36,18 +35,21 @@ $(document).ready(function(){
     $('#added1').remove();
     $('#added2').remove();
     $('<i id = "added2"> That\'s perfect, ' + textInput + '. Please proceed.</i>').insertAfter("#aged");
+    //Teases the user with comments in the age question
 
   });
 
   $("form").on("submit", function(event) {
        event.preventDefault();
+       //Prevents the form from being submitted and executes the following code instead.
+       var textInput = $("#name").val();
+       var userInput = [];
 
-       //$(.container){$(this).toggle()};
-
-       $('.all').append('<div class="suggestion"></div>')
-
+       $($('.all').append('<div class="suggestion"><h3>Here are your suggestions '+ textInput+'!</h3></div>')).fadeIn()
+       //Creates a new suggestion div with only a header for the user results using the user name.
 
 
+       //The following vars gather the answers from the radio buttons in each question and push them to the userINput array.
        var ageVal = $("input[name='age']:checked").val();
        userInput.push(Number(ageVal))
 
@@ -67,11 +69,12 @@ $(document).ready(function(){
        userInput.push(Number(terrainVal))
 
 
-
+       //The following arrays will be used to store the occurences of a, b, and c as the answers to the questions
        var a = [];
        var b = [];
        var c = [];
 
+       //This function sorts the userInput array and pushes the occurences of answers as a, b, or c to their respective arrays.
        function instanceCount(array){
          for(i=0; i<= array.length; i++){
            if (array[i] === 1){a.push(array[i])};
@@ -80,148 +83,70 @@ $(document).ready(function(){
          };
 
        };
-
-
        instanceCount(userInput);
 
 
-              /*if(a.length > b.length && a.length > c.length){
-                alert("A is da longest")
-              };*/
-
-
-              if(a.length > b.length && a.length > c.length){
-
-                $('.suggestion').append('<p>You chose mostly a\'s so we think you might need some solitude.</p>');
-                $('.suggestion').append('<img class = "images" id="solo1" src="img/solo1.jpg"/><br>');
-                $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><br>');
-              };
-
-
-
-              if(b.length > a.length && b.length > c.length){
-                $('.suggestion').append('<p>You chose mostly b\'s so we think you might travelling with family.</p>');
-                $('.suggestion').append('<img class = "images" id="Family2" src="img/Family2.jpg"/><br>');
-                $('.suggestion').append('<img class = "images" id="Family1" src="img/Family1.jpg"/><br>');
-                $('.suggestion').append('<p>But just in case you are not, you could try here instead.</p>');
-                $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><br>');
-
-              };
-
-              if(c.length> a.length && c.length>b.length){
-                $('.suggestion').append('<p>You chose mostly c\'s so we think you might be looking for a party. We know some great places.</p>');
-                $('.suggestion').append('<img class = "images" id="Party1" src="img/Party1.jpg"/><br>');
-                $('.suggestion').append('<img class = "images" id="Party2" src="img/Party2.jpg"/><br>');
-                $('.suggestion').append('<p>But just in case you are not, you could try here instead.</p>');
-                $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><br>');
-
-              };
-
-              if(a.length === b.length && a.length === c.length){
-                $('.suggestion').append('<p>You chose an equal number of a\'s, b\'s and c\'s, so we\'ll just go ahead and give you ALL these suggestions.</p>');
-                $('.suggestion').append('<p>If solitude was what you were looking for, then try this.</p>');
-                $('.suggestion').append('<img class = "images" id="solo1" src="img/solo1.jpg"/><br>');
-                $('.suggestion').append('<p>If you\'re looking for a party, try this.</p>');
-                $('.suggestion').append('<img class = "images" id="Party2" src="img/Party2.jpg"/><br>');
-                $('.suggestion').append('<p>If you\'re travelling with family, try this.</p>');
-                $('.suggestion').append('<img class = "images" id="Family2" src="img/Family2.jpg"/><br>');
-              };
-
-              if(a.length === b.length && a.length > c.length){
-                $('.suggestion').append('<p>You chose an equal number of a\'s and b\'s so we\'re not sure if you\'re alone or with family.<br>These are appropriate for both</p>');
-                $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><br>');
-                $('.suggestion').append('<img class = "images" id="Family1" src="img/Family1.jpg"/><br>');
-              };
-
-              if(a.length === c.length && a.length > b.length){
-                $('.suggestion').append('<p>You chose an equal number of a\'s and c\'s so we think you want to get away from everyone for a good time.<br>These might be appropriate.</p>');
-                $('.suggestion').append('<img class = "images" id="solo1" src="img/solo1.jpg"/><br>');
-                $('.suggestion').append('<img class = "images" id="Party2" src="img/Party2.jpg"/><br>');
-              };
-
-              if(b.length === c.length && b.length > a.length){
-                $('.suggestion').append('<p>You chose an equal number of b\'s and c\'s so we think you want to get away from everyone to relax with family.<br>These might be appropriate.</p>');
-                $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><br>');
-                $('.suggestion').append('<img class = "images" id="Family2" src="img/Family2.jpg"/><br>');
-              };
-
-
-
-              $('.containerone').remove()
-
-
-       /*var adder = (
-         Number(ageVal)  + Number(weatherVal) + Number(nightVal) + Number(longVal) +
-         Number(companyVal) + Number(terrainVal)
-       );
-
-       $('body').append("<div class=\"suggestion\"></div>")
-       $('.suggestion').append("<h3>Here are your suggestions!</h3>");
-
-
-       if (adder <= 9) {
-         $('.suggestion').append('<p>You sound like you might need some solitude.</p>');
-         $('.suggestion').append('<img id="solo1" src="img/solo1.jpg" />');
-         $('.suggestion').append('<img id="solo2" src="img/solo2.jpg" />');
-       };
-
-       if ( adder>7 && adder <=9 ){
-         $('.suggestion').append('<p>If you want some anonymous company, you could try here too.</p>');
-         $('.suggestion').append('<img id="theImg" src="img/Family1.jpg" />');
-       };
-
-       if(adder>9 && adder <12){
-         $('.suggestion').append('<p>You sound like you are travelling with family.</p>');
-         $('.suggestion').append('<img id="Family2" src="img/Family2.jpg" />');
-         $('.suggestion').append('<img id="Family1" src="img/Family1.jpg" />');
-         $('.suggestion').append('<p>But just in case you are not, you could try here too.</p>');
-         $('.suggestion').append('<img id="solo2" src="img/solo2.jpg" />');
-
-       };
-
-       if(adder===12){
-         $('.suggestion').append('<p>You sound like you are travelling with family.</p>');
-         $('.suggestion').append('<img id="Family2" src="img/Family2.jpg" />');
-         $('.suggestion').append('<img id="Family1" src="img/Family1.jpg" />');
-
-       };
-
-       if(adder>12 && adder <=15){
-         $('.suggestion').append('<p>You sound like you are travelling with family.</p>');
-         $('.suggestion').append('<img id="Family2" src="img/Family2.jpg" />');
-         $('.suggestion').append('<p>But just in case you are not, you could try here too.</p>');
-         $('.suggestion').append('<img id="Party1" src="img/Party1.jpg" />');
-
-       };
-
-       if(adder>15){
-         $('.suggestion').append('<p>Looking for a party? We know some great places.</p>');
-         $('.suggestion').append('<img id="Party1" src="img/Party1.jpg" />');
-         $('.suggestion').append('<img id="Party2" src="img/Party2.jpg" />');
-       };
-
-
-       $('.container').remove()
-
+       /*The following if statements use the user results(the number of occurences of a,b or c) as guidance and then append to a the
+       suggestion div the recommendations in the form of an image and an address.
        */
 
+       if(a.length > b.length && a.length > c.length){
+
+         $('.suggestion').append('<p>You chose mostly a\'s so we think you might need some solitude.</p>');
+         $('.suggestion').append('<img class = "images" id="solo1" src="img/solo1.jpg"/><span class = "places">Moab, Utah, USA</span><br>');
+         $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><span class = "places">Siargao, Philippines</span><br>');
+       };
 
 
+       if(b.length > a.length && b.length > c.length){
+         $('.suggestion').append('<p>You chose mostly b\'s so we think you might travelling with family.</p>');
+         $('.suggestion').append('<img class = "images" id="Family2" src="img/Family2.jpg"/><span class = "places">The Balearic island of Majorca</span><br>');
+         $('.suggestion').append('<img class = "images" id="Family1" src="img/Family1.jpg"/><span class = "places">Sunny beach Bulgaria</span><br>');
+         $('.suggestion').append('<p>But just in case you are not, you could try here instead.</p>');
+         $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><span class = "places">Siargao, Philippines</span><br>');
 
+       };
+
+       if(c.length> a.length && c.length>b.length){
+         $('.suggestion').append('<p>You chose mostly c\'s so we think you might be looking for a party. We know some great places.</p>');
+         $('.suggestion').append('<img class = "images" id="Party1" src="img/Party1.jpg"/><span class = "places">New Orleans, Louisiana, USA</span><br>');
+         $('.suggestion').append('<img class = "images" id="Party2" src="img/Party2.jpg"/><span class = "places">Ibiza Sunset Boat Party</span><br>');
+         $('.suggestion').append('<p>But just in case you are not, you could try here instead.</p>');
+         $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><span class = "places">Siargao, Philippines</span><br>');
+
+       };
+
+       if(a.length === b.length && a.length === c.length){
+         $('.suggestion').append('<p>You chose an equal number of a\'s, b\'s and c\'s, so we\'ll just go ahead and give you ALL these suggestions.</p>');
+         $('.suggestion').append('<p>If solitude was what you were looking for, then try this.</p>');
+         $('.suggestion').append('<img class = "images" id="solo1" src="img/solo1.jpg"/><span class = "places">Moab, Utah, USA</span><br>');
+         $('.suggestion').append('<p>If you\'re looking for a party, try this.</p>');
+         $('.suggestion').append('<img class = "images" id="Party2" src="img/Party2.jpg"/><span class = "places">Ibiza Sunset Boat Party</span><br>');
+         $('.suggestion').append('<p>If you\'re travelling with family, try this.</p>');
+         $('.suggestion').append('<img class = "images" id="Family2" src="img/Family2.jpg"/><span class = "places">The Balearic island of Majorca</span><br>');
+       };
+
+       if(a.length === b.length && a.length > c.length){
+         $('.suggestion').append('<p>You chose an equal number of a\'s and b\'s so we\'re not sure if you\'re alone or with family.<br>These are appropriate for both</p>');
+         $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><span class = "places">Siargao, Philippines</span><br>');
+         $('.suggestion').append('<img class = "images" id="Family1" src="img/Family1.jpg"/><span class = "places">Sunny beach Bulgaria</span><br>');
+       };
+
+       if(a.length === c.length && a.length > b.length){
+         $('.suggestion').append('<p>You chose an equal number of a\'s and c\'s so we think you want to get away from everyone for a good time.<br>These might be appropriate.</p>');
+         $('.suggestion').append('<img class = "images" id="solo1" src="img/solo1.jpg"/><span class = "places">Moab, Utah, USA</span><br>');
+         $('.suggestion').append('<img class = "images" id="Party2" src="img/Party2.jpg"/><span class = "places">Ibiza Sunset Boat Party</span><br>');
+       };
+
+       if(b.length === c.length && b.length > a.length){
+         $('.suggestion').append('<p>You chose an equal number of b\'s and c\'s so we think you want to get away from everyone to relax with family.<br>These might be appropriate.</p>');
+         $('.suggestion').append('<img class = "images" id="solo2" src="img/solo2.jpg"/><span class = "places">Siargao, Philippines</span><br>');
+         $('.suggestion').append('<img class = "images" id="Family2" src="img/Family2.jpg"/><span class = "places">The Balearic island of Majorca</span><br>');
+       };
+
+       $('.containerone').fadeOut()
+       //Fades out the form while the suggestions replace it.
 
     });
 
 });
-
-
-
-/*
-Solo1 = Moab, Utah, USA (really alone)
-Solo2 = Siargao, Philippines (alone with space for company)
-
-Family1 = Sunny beach Bulgaria (Everyone friendly)
-Family2 = The Balearic island of Majorca (Fam hard)
-
-Party1 = New Orleans, Louisiana, USA (family friendly)
-Party2 = Ibiza Sunset Boat Party (Party hard)
-*/
